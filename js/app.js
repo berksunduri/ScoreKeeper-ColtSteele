3441bbd9c6b35c0fe2602684cbff95703434da06
Ololade <lolly@gmail.com>
2024-05-18 16:10:15 +0200

    refactoring code, creating functions and stuff

--- js/app.js
+++ js/app.js
@@ -9,41 +9,49 @@ let p2Score = 0;
 let gametype = parseInt(select.value);
 
 select.addEventListener('change', function() {
-    console.log("CHANGED!!")
     gametype = parseInt(select.value);
-    console.log(`value of gametype:${gametype}`)
 });
 
-function resetGame() {
-    p1Score = 0;
-    p2Score = 0;
+function updateScore() {
     score.textContent = `${p1Score} to ${p2Score}`;
 }
 
-p1.addEventListener('click', function() {
-    console.log(`value of gametype:${gametype}`)
-    if (p1Score < gametype && p2Score < gametype) {
-        p1Score++;
-        score.innerText = `${p1Score} to ${p2Score}`;
-        if (p1Score === gametype) {
-            setTimeout(() => alert("Player 1 won!!!!!!"), 0);
-            setTimeout(() =>resetGame(), 0);
-        }
+function checkWinner() {
+    if (p1Score === gametype) {
+        setTimeout(() => alert("Player 1 won!!!!!!"), 0);
+        setTimeout(() => resetGame(), 0);
+    } else if (p2Score === gametype) {
+        setTimeout(() => alert("Player 2 won!!!!!!"), 0);
+        setTimeout(() => resetGame(), 0);
     }
+}
+
+function handleClick(player) {
+    if (p1Score < gametype && p2Score < gametype) {
+        if (player === 'p1') {
+            p1Score++;
+        } else if (player === 'p2') {
+            p2Score++;
+        }
+        updateScore();
+        checkWinner();
+    }
+}
+
+p1.addEventListener('click', function() {
+    handleClick('p1');
 });
 
 p2.addEventListener('click', function() {
-    console.log(`value of gametype:${gametype}`)
-    if (p1Score < gametype && p2Score < gametype) {
-        p2Score++;
-        score.textContent = `${p1Score} to ${p2Score}`;
-        if (p2Score === gametype) {
-            setTimeout(() => alert("Player 2 won!!!!!!"), 0);
-            setTimeout(() =>resetGame(), 0);
-        }
-    }
+    handleClick('p2');
 });
 
 reset.addEventListener('click', function() {
     resetGame();
 });
+
+function resetGame() {
+    p1Score = 0;
+    p2Score = 0;
+    updateScore();
+}
